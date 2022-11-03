19608a52fa12ca87cb1da0c25c6eac8b0ece652e
juliasavicke <juliasavicke@gmail.com>
2022-11-03 09:24:05 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,20 @@
+"use strict";
+console.log("main.js");
+
+const numbers = [
+  1, 2, 3, 5.3, 8, -8, -5.5, -6, -16, -5, -9, 4.2, 5, 12, -6, 45,
+];
+
+// 1. Atrinkti skaicius didesnius uz 3 => filter
+
+// 2. Noretumem grazinti nauja masyva su padvigubintom reiksmem => map
+
+// 3. Noretumem gauti neigiamu reiksmiu bendra suma => filter => reduce
+
+// 4. Suzinoti, ar yra skaicius 5 musu numbers masyve => includes
+
+// 5. Consoleje atspausdinti visas reiksmes su indeksais => forEach
+
+// 6. every
+
+// 7. some
