633242ff8d36db60c9bbd922dccf911dca1d460b
juliasavicke <juliasavicke@gmail.com>
2022-11-04 08:48:30 +0200

    delete btn to remove a person from the list

--- js/people.js
+++ js/people.js
@@ -134,6 +134,8 @@ let people = [
   },
 ];
 
+const pplContainer = document.getElementById("container");
+
 // darydami uzduotis nekeiciame orginalo iskyrus 8 uzduoti
 
 // 1. atspausdinti visus vyrus
@@ -215,6 +217,12 @@ console.log("addId ===", addId);
 // 8.1 persikopijuoti kad orginalas butu su id reiksmem
 
 // 9. sukurti funkcija kuri gaudama id istrina ta masyvo elementa. deletPerson(p_02)
+
+function deletePerson(id) {
+  people = people.filter((person) => person.id != id);
+  renderList(people, pplContainer);
+}
+
 // 10. parasyti funkcija kuri sukuria ir grazina viena zmogaus kortele tokiu pavidalu
 // `
 // <article class="card">
@@ -226,7 +234,36 @@ console.log("addId ===", addId);
 // `;
 // jei vaiduorja masinos ikona zalia, jei ne raudona
 
+function makePerson({ name, surname, sex, age, income, hasCar, id }) {
+  const articleEl = document.createElement("article");
+  articleEl.className = "card";
+  articleEl.dataset.pid = id;
+  articleEl.innerHTML = `
+    <h3>${name} ${surname}</h3>
+    <h4>${age} metu ${sex === "male" ? "vyras" : "moteris"}</h4>
+    <p>atlyginimas: ${income} eur</p>
+    <p class="driver-icon ${
+      hasCar ? "driver" : ""
+    }"><i class="fa fa-car" aria-hidden="true"></i></p>
+    <button id=delete-btn>Delete</button>`;
+
+  const deleteBtnEl = articleEl.querySelector("#delete-btn");
+  deleteBtnEl.addEventListener("click", (event) => {
+    deletePerson(id);
+    //event.target.parentElement.remove(); //siuo atveju reiketu ir people sarasa atnaujinti - pasalinti is jo istrinta elementa.
+  });
+  return articleEl;
+}
+
 // 11. sukurti funkcija renderList(data) kuri is dataArr sugeneruoja elementus su 10pratimo funkcija ir patalpina juos i `<div id="people-container" class="people-container">`
+function renderList(arr, dest) {
+  const htmlElsFromArr = arr.map(makePerson);
+  dest.innerHTML = "";
+  htmlElsFromArr.forEach((el) => {
+    dest.append(el);
+  });
+}
+renderList(people, pplContainer);
 
 // 12. su 11to pratimo funkcija rikiuojam ir atrenkam elementus i sarasa mygtuku paspaudimais.
 // `
